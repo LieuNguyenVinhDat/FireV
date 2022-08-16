9ecccd50e438f46717e18e740748993976088fd8
manhvipro123 <manhvp1810@gmail.com>
2022-08-16 11:39:01 +0700

    edit folder structure

--- fire-v_server/src/app.module.ts
+++ fire-v_server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, VideoModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- fire-v_server/src/main.ts
+++ fire-v_server/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const sv = 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(sv);
+  console.log(`sever is running on: http://127.0.0.1:${sv}/`)
+
 }
 bootstrap();
