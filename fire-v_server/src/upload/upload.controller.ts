9ae7bacf767ce6c9864bf9178b310ebec2a3eb59
NguyenBinh <binh.nt2064@sinhvien.hoasen.edu.vn>
2022-08-23 15:05:30 +0700

    push server th nha

--- fire-v_server/src/upload/upload.controller.ts
+++ fire-v_server/src/upload/upload.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { UploadService } from './upload.service';
-
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+  @Post('add')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: 'src/uploads/videos',
+      filename: (req, file, cb) => {
+        // Generating a 32 random chars long string
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+        //Calling the callback passing the random name generated with the original extension name
+        cb(null, `${randomName}${extname(file.originalname)}`)
+      }
+    })
+  }))
+  async upload( @UploadedFile() file) {
+    console.log(file)
+  }
 }
