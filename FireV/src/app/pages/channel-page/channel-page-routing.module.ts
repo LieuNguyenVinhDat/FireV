9c598fd1f42936a9ac842b31b57717dc34320114
vinhdat <dat.lnv1365@sinhvien.hoasen.edu.vn>
2022-08-26 17:12:45 +0700

    luxubu

--- FireV/src/app/pages/channel-page/channel-page-routing.module.ts
+++ FireV/src/app/pages/channel-page/channel-page-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChannelComponent } from 'src/app/components/channel/channel.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { ChannelPageComponent } from './channel-page.component';
 
 const routes: Routes = [
-  { path: '', component: ChannelComponent },
+  { path: '', component: ChannelPageComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'add', loadChildren: () => import('../channel-page/add-page/add-page.module').then(m => m.AddPageModule)},
   { path: 'view_profile', loadChildren: () => import('../channel-page/view-profile-page/view-profile-page.module').then(m => m.ViewProfilePageModule)},
 
